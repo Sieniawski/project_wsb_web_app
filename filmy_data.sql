376b4a27bdf26ebd830749f4c2e0427c80641b57
Wojciech <sieniawski7@wp.pl>
2019-06-27 14:40:38 +0200

    add sql for movies

---
+++ filmy_data.sql
@@ -0,0 +1,15 @@
+﻿create table filmy(id_filmu int, tytul varchar(40), rok_produkcji int, cena real);
+
+insert into filmy values (1,'Wesele',2004, 10);
+insert into filmy values (2,'Ghostbusters',1984, 5.5);
+insert into filmy values (3,'Terminator',1984, 8.5);
+insert into filmy values (4,'Taksowkarz', 1976, 5);
+insert into filmy values (5,'Pluton',1986, 5);
+insert into filmy values (6,'Frantic',1988, 8.5);
+insert into filmy values (7,'Ronin',1998, 9.5);
+insert into filmy values (8,'Depresja gangstera',1999, 10.5);
+insert into filmy values (9,'Leon zawodowiec',1994, 8.5);
+insert into filmy values (10,'Mission Impossible',1996, 8.5);
+
+
+select * from filmy;
